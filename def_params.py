66b9f4b2823d79ef0bda2aa711f5bd9ad1d818dd
kefirr85 <166715341+kefirr85@users.noreply.github.com>
2024-04-30 15:03:55 +0300

    Add files via upload

---
+++ def_params.py
@@ -0,0 +1,10 @@
+def print_params(car, numb):
+    print(car, numb)
+    print(car, numb)
+print_params('Mercedes', 'o342oo777')
+print_params('BMW', 'k500kk77')
+def print_params(list):
+    list = [1,2,3,4,5,6,7,8,9]
+    print(f'numb', list)
+    print(f'bob', list)
+print_params(list)
